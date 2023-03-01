9b31999fed895f5083345da3904360226c8a3b91
EtaYozhik <carter.singh@hotmail.com>
2023-03-01 13:09:09 +0000

    Did a comment

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import Navbar from './Navbar'
 import Footer from './Footer'
 import Content from './Content'
-import Images from './Images'
-import square from './square.jpeg'
+//import Images from './Images'
+//import square from './square.jpeg'
 const cars =['TIN','FRESH','FROZEN','COOKED'];
 
 function App() {
   return (
     <div className='App'>
       <Navbar/>
+
       <Content/> 
       <Footer/>
+      
 
     </div>
       
